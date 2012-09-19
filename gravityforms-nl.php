6fd3427c886ba4f1e67cfcc59ba85a412ea6c1e4
Remco Tolsma <info@remcotolsma.nl>
2012-09-19 08:58:11 +0200

    Updated to Signature 1.2.

--- gravityforms-nl.php
+++ gravityforms-nl.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gravity Forms (nl)
 Plugin URI: http://pronamic.eu/wp-plugins/gravityforms-nl/
-Description: Extends the Gravity Forms plugin and add-ons with the Dutch language: <strong>Gravity Forms</strong> 1.6.7 | <strong>User Registration Add-On</strong> 1.4 | <strong>Campaign Monitor Add-On</strong> 2.0 | <strong>MailChimp Add-On</strong> 1.7 | <strong>PayPal Add-On</strong> 1.5 | <strong>Signature Add-On</strong> 1.1 | <strong>Polls Add-On</strong> 1.0.beta1
+Description: Extends the Gravity Forms plugin and add-ons with the Dutch language: <strong>Gravity Forms</strong> 1.6.7 | <strong>User Registration Add-On</strong> 1.4 | <strong>Campaign Monitor Add-On</strong> 2.0 | <strong>MailChimp Add-On</strong> 1.7 | <strong>PayPal Add-On</strong> 1.5 | <strong>Signature Add-On</strong> 1.2 | <strong>Polls Add-On</strong> 1.0.beta1
 
 Version: 2.6.10
 Requires at least: 3.0
